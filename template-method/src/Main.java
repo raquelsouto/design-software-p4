71bb1603bf92401479031417be0e046e37a070fe
Raquel Souto <kelsouto@gmail.com>
2021-10-20 21:11:53 -0300

    Adicionar teste

--- template-method/src/Main.java
+++ template-method/src/Main.java
@@ -8,13 +8,23 @@ public class Main {
         Icpp icpp = new Icpp(1500, item1);
         Ikcv ikcv = new Ikcv(120, item2);
 
+        System.out.println("ICPP");
+        double resultado1 = icpp.calculaImposto(1500, item1);
+        System.out.printf("Imposto R$ " + "%.2f", resultado1);
 
-        //ICPP
-//        boolean result = icpp.calcularComImpostoMaximo(1500, item1);
-//        System.out.println(result);
+        System.out.println();
+        double resultado2 = icpp.calculaImposto(10000, item1);
+        System.out.printf("Imposto R$ " + "%.2f", resultado2);
 
-        double result = icpp.calculaImposto(10000, item1);
-        System.out.println(result);
+        System.out.println("\n");
 
+        System.out.println("IKCV");
+        double result = ikcv.calculaImposto(360, item2);
+        System.out.printf("Imposto R$ " + "%.2f", result);
+
+        System.out.println();
+        double result2 = ikcv.calculaImposto(4500, item1);
+        System.out.printf("Imposto R$ " + "%.2f", result2);
+    }
 
 }
